9b41cf432e303b588064a5bfa0184d0bbccef2bf
Olivier St-Amand <olivier.st.amand.1@gmail.com>
2024-05-31 19:50:54 -0400

    Add lr scheduler and bigger model

--- ddpm-bird/train_simple.py
+++ ddpm-bird/train_simple.py
@@ -3,11 +3,14 @@ import argparse
 import subprocess
 from pathlib import Path
 import json
+import math
 
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision.io import read_image
 from torchvision.transforms import v2
+from torch.optim.lr_scheduler import LambdaLR
+
 
 from dotenv import load_dotenv
 import pandas as pd
@@ -18,6 +21,49 @@ import wandb
 from utils import generate_random_samples, generate_image_from_samples
 
 load_dotenv()
+
+
+# from: https://github.com/huggingface/diffusers/blob/05be622b1c152bf11026f97f083bb1b989231aec/src/diffusers/optimization.py#L154-L185
+def get_cosine_schedule_with_warmup(
+    optimizer,
+    num_warmup_steps: int,
+    num_training_steps: int,
+    num_cycles: float = 0.5,
+    last_epoch: int = -1,
+) -> LambdaLR:
+    """
+    Create a schedule with a learning rate that decreases following the values of the cosine function between the
+    initial lr set in the optimizer to 0, after a warmup period during which it increases linearly between 0 and the
+    initial lr set in the optimizer.
+
+    Args:
+        optimizer ([`~torch.optim.Optimizer`]):
+            The optimizer for which to schedule the learning rate.
+        num_warmup_steps (`int`):
+            The number of steps for the warmup phase.
+        num_training_steps (`int`):
+            The total number of training steps.
+        num_periods (`float`, *optional*, defaults to 0.5):
+            The number of periods of the cosine function in a schedule (the default is to just decrease from the max
+            value to 0 following a half-cosine).
+        last_epoch (`int`, *optional*, defaults to -1):
+            The index of the last epoch when resuming training.
+
+    Return:
+        `torch.optim.lr_scheduler.LambdaLR` with the appropriate schedule.
+    """
+
+    def lr_lambda(current_step):
+        if current_step < num_warmup_steps:
+            return float(current_step) / float(max(1, num_warmup_steps))
+        progress = float(current_step - num_warmup_steps) / float(
+            max(1, num_training_steps - num_warmup_steps)
+        )
+        return max(
+            0.0, 0.5 * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress))
+        )
+
+    return LambdaLR(optimizer, lr_lambda, last_epoch)
 
 
 class BirdDataset(Dataset):
@@ -68,26 +114,31 @@ class BirdDataset(Dataset):
         return image
 
 
-def build_model(image_size: int = 64):
+def build_model(image_size: int = 64, dp: float = 0.):
     return UNet2DModel(
-        sample_size=image_size,
-        in_channels=3,
-        out_channels=3,
-        layers_per_block=2,
-        block_out_channels=(64, 128, 128, 256),
-        down_block_types=(
-            "DownBlock2D",
-            "DownBlock2D",
-            "AttnDownBlock2D",
-            "AttnDownBlock2D",
-        ),
-        up_block_types=(
-            "AttnUpBlock2D",
-            "AttnUpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-        ),
-    )
+            sample_size=image_size,
+            in_channels=3,
+            out_channels=3,
+            layers_per_block=2,
+            dropout=dp,
+            block_out_channels=(128, 128, 256, 256, 512, 512),
+            down_block_types=(
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "AttnDownBlock2D",
+                "DownBlock2D",
+            ),
+            up_block_types=(
+                "UpBlock2D",
+                "AttnUpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+            ),
+        )
 
 
 def fit(
@@ -99,18 +150,31 @@ def fit(
     num_train_timesteps: int = 1000,
     image_size: int = 32,
     lr=4e-4,
-    batch_pg=True,
-    out_dir = "out",
     seed: int = 42,
-    **kwargs
+    batch_pg=True,
+    out_dir="out",
+    **kwargs,
 ):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.set_default_device(device)
     model.to(device)
 
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        generator=torch.Generator(device=device),
+    )
     log_each_batch = max(len(dataloader) // 5, 1)
 
     opt = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    lr_scheduler = get_cosine_schedule_with_warmup(
+        opt,
+        num_warmup_steps=int(len(dataloader) * epochs * 0.33), 
+        num_training_steps=(len(dataloader) * epochs),
+    )
+
     lossf = torch.nn.MSELoss()
 
     losses = []
@@ -119,6 +183,7 @@ def fit(
     for epoch in range(epochs):
         for batch_i, x in enumerate(dataloader):
             x = x.to(device)
+
             bs = x.shape[0]
 
             noise = torch.randn(x.shape).to(device)
@@ -133,35 +198,44 @@ def fit(
 
             loss.backward()
             opt.step()
+            lr_scheduler.step()
             opt.zero_grad()
 
             if batch_pg and (batch_i + 1) % log_each_batch == 0:
-                print(f"status: epoch {epoch+1}, batch {batch_i+1}/{len(dataloader)}")
+                print(f"status: epoch {epoch+1}, batch {batch_i+1:.2f}/{len(dataloader)}")
 
         # log epoch loss
         loss_avg = sum(losses[-len(dataloader) :]) / len(dataloader)
-        print(f"done: epoch {epoch+1}, loss {loss_avg}")
 
         # generate some images
         model.eval()
-        samples = generate_random_samples(model, noise_scheduler, n=4, image_size=image_size, seed=seed)
-        img = generate_image_from_samples(samples, resize=image_size*2)
+        samples = generate_random_samples(
+            model, noise_scheduler, n=4, image_size=image_size, seed=seed, pg=False
+        )
+        img = generate_image_from_samples(samples)
         img.save(Path(out_dir) / f"sample_{epoch+1}.jpg")
         model.train()
 
-        wandb.log({
-            "loss": loss_avg,
-            "sample": wandb.Image(img)
+        # current lr
+        last_lr = lr_scheduler.get_last_lr()[0]
+
+        wandb.log(
+            {
+                "loss": loss_avg,
+                "sample": wandb.Image(img),
+                "lr": last_lr,
             }
         )
+
+        print(f"done: epoch {epoch+1}, loss {loss_avg}, lr {last_lr}")
 
     return losses
 
 
 def save_outs(
-    out_dir: Path, args: dict[str, any], model: UNet2DModel, train_losses: list
+    out_dir: Path, config: dict[str, any], model: UNet2DModel, train_losses: list
 ):
-    out = {"args": args, "train_losses": train_losses}
+    out = {"config": config, "train_losses": train_losses}
     if not out_dir.exists():
         os.makedirs(out_dir)
     torch.save(model.state_dict(), out_dir / "state_dict_model.pt")
@@ -197,8 +271,8 @@ def main(args):
     for _ in tqdm(dataloader):
         pass
 
-    # train
-    model = build_model(image_size=args.image_size)
+    # get ready for training, create model, dataset & scheduler
+    model = build_model(image_size=args.image_size, dp=args.dp)
 
     dataset = BirdDataset(
         data_dir,
@@ -210,21 +284,21 @@ def main(args):
         num_train_timesteps=args.num_train_timesteps, beta_schedule="squaredcos_cap_v2"
     )
 
-    # wandb
+    # populate configs
+    config = vars(args)
+    config["train_steps"] = math.ceil(len(dataset) / args.bs) * args.epochs
+
+    # wandb init
     wandb.init(
-        project="ddpm-bird-32px",
-        config=vars(args),
+        project=f"ddpm-bird-{args.image_size}px",
+        config=config,
     )
 
-    train_losses = fit(
-        model,
-        dataset,
-        noise_scheduler,
-        **vars(args)
-    )
+    # train model
+    train_losses = fit(model, dataset, noise_scheduler, **vars(args))
 
     # save outputs
-    save_outs(out_dir, vars(args), model, train_losses)
+    save_outs(out_dir, config, model, train_losses)
 
 
 def parse_args():
@@ -236,19 +310,31 @@ def parse_args():
     parser.add_argument("--batch-pg", action="store_true")
     parser.add_argument("--out-dir", type=str, default="out")
     parser.add_argument("--num-train-timesteps", type=int, default=1000)
-    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--lr", type=float, default=4e-4)
     parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--dp", type=float, default=0.)
     args = parser.parse_args()
     print(f"running with: {vars(args)}")
     return args
-
 
 """ 
 References:
 
 - BIRDS 525 SPECIES- IMAGE CLASSIFICATION: https://www.kaggle.com/datasets/gpiosenka/100-bird-species
+    - images are 224x224
 - https://colab.research.google.com/github/huggingface/diffusion-models-class/blob/main/unit1/01_introduction_to_diffusers.ipynb
 - https://github.com/huggingface/diffusers/blob/main/examples/unconditional_image_generation/train_unconditional.py
+- Denoising Diffusion Probabilistic Models: https://arxiv.org/pdf/2006.11239
+    - Our 32 × 32 models use four feature map resolutions (32 × 32 to 4 × 4), and our 256 × 256 models use six
+    - All models have two convolutional residual blocks
+        per resolution level and self-attention blocks at the 16 × 16 resolution between the convolutional
+        blocks [6]
+    - We set the dropout rate on CIFAR10 to 0.1 by sweeping over the values {0.1, 0.2, 0.3, 0.4}.
+    We trained on CelebA-HQ for 0.5M steps, LSUN Bedroom for 2.4M steps, LSUN Cat for 1.8M steps, and LSUN Church for 1.2M steps. The larger LSUN Bedroom model was trained for 1.15M steps.
+    - We used random horizontal flips during training for CIFAR10;
+    - We set the learning rate to 2 × 10−4 without any sweeping, and we lowered it to 2 × 10−5 for the 256 × 256 images
+    - We set the batch size to 128 for CIFAR10 and 64 for larger images. We did not sweep over these values.
+    - We used EMA on model parameters with a decay factor of 0.9999. We did not sweep over this value.
 
 Setup:
 
@@ -256,12 +342,17 @@ Setup:
 
 Examples:
 
-run test training sample: 
-    python ddpm-bird/train_simple.py --epochs 10 --subset 1000 --out out/test
+run test training on subset of data, 32px
+    python ddpm-bird/train_simple.py --epochs 5 --subset 1000 --out out/test
 
-run 5 epochs all data: 
+run 5 epochs, all data: 
     python ddpm-bird/train_simple.py --epochs 5 --out out/ddpm-bird/epochs-5
 
+run 30 epochs, all data, 64px
+    python ddpm-bird/train_simple.py --epochs 30 --image-size 64 --b 256 --out out/ddpm-bird/64px-epochs-30 --lr 1e-4
+
+run 30 epochs, all data, 224px
+    python ddpm-bird/train_simple.py --epochs 30 --image-size 224 --b 64 --out out/ddpm-bird/224px-epochs-30 --lr 1e-4
 """
 if __name__ == "__main__":
     args = parse_args()
--- ddpm-bird/utils.py
+++ ddpm-bird/utils.py
@@ -11,12 +11,14 @@ import torchvision
 from torchvision.transforms import v2
 
 
-def generate_random_samples(model, noise_scheduler, n: int, image_size=32, seed: int = None):
+def generate_random_samples(
+    model, noise_scheduler, n: int, image_size=32, seed: int = None, pg: bool = True
+):
     seed = seed if seed is not None else randint(0, sys.maxsize)
     torch.manual_seed(seed)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     sample = torch.randn(n, 3, image_size, image_size).to(device)
-    for t in tqdm(noise_scheduler.timesteps):
+    for t in tqdm(noise_scheduler.timesteps, disable= not pg):
         with torch.no_grad():
             residual = model(sample, t).sample
         sample = noise_scheduler.step(residual, t, sample).prev_sample
@@ -26,12 +28,9 @@ def generate_random_samples(model, noise_scheduler, n: int, image_size=32, seed:
 def generate_image_from_samples(x, resize=None):
     """Given a batch of images x, make a grid and convert to PIL"""
     x = x * 0.5 + 0.5  # Map from (-1, 1) back to (0, 1)
-
     if resize:
         x = v2.Resize((resize, resize))(x)
-
     grid = torchvision.utils.make_grid(x)
     grid_im = grid.detach().cpu().permute(1, 2, 0).clip(0, 1) * 255
     grid_im = Image.fromarray(np.array(grid_im).astype(np.uint8))
-
     return grid_im
